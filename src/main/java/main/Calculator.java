ddbc3f8fc6141a821979f11e6d08a027a531e76b
oreo <oreo@DESKTOP-59U8MHI>
2019-03-31 06:20:26 -0700

    done

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -37,28 +37,28 @@ public class Calculator {
     }
     public static int add(int x, int y)
     {
-    	int result=0;
-//        write your code here 
+    	//int result=0;
+    	 
     	
-        return result;
+        return (x+y);
     }
     public static int sub(int x, int y)
     {
-    	int result=0;
+    	//int result=0;
 //        write your code here
-        return result;
+        return (x-y);
     }
     public static int mult(int x, int y)
     {
-    	int result=0;
+    	//int result=0;
 //      write your code here        
-    	return result;
+    	return x*y;
     }
     public static int div(int x, int y)
     {
-    	int result=0;
+    	//int result=0;
 //      write your code here
-    	return result;
+    	return x/y;
     }
 
 }
